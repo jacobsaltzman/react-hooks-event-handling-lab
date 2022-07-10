071875f9be5f749f2e465a45eee53cf44c90c5ca
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-07-10 00:02:45 -0700

    commit tree 14b89037820f433cd137453e38c844f43590051b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+  function focusPhrase(){ console.log("Good!")};
+  function blurPhrase(){ console.log("Hey! Eyes on me!")};
+  return <button onFocus={focusPhrase} onBlur={blurPhrase}> Eyes on me </button>
+}
+export default EyesOnMe;
